8560d3803ee5417b9600d2b2237e4a0a73850b48
bird4125 <39263545+bird4125@users.noreply.github.com>
2019-05-12 20:07:33 +0900

    ADD QUEST
    DD

--- 01A-1/script/menu.js
+++ 01A-1/script/menu.js
@@ -1,18 +1,2 @@
-$(document).ready(function () {
-    var title = $("#head_sec .head_ri .menus a");
-    var subtitle = $("#head_sec .head_ri .sub_me ul");
-    title.mouseover(function(){
-        title.animate({
-            color : "#faf"
-        },500)
-        subtitle.animate({
-            bottom : "-150px"
-        },500);
-    });
-    title.mouseout(function(){
-        subtitle.animate({
-            bottom : "-0px"
-        },500);
-    });
-    
+$(document).ready(function () {    
 })
