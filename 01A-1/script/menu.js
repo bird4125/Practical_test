f5a6dc527009a76af517b26a2f1a230ae41875f6
bird4125 <39263545+bird4125@users.noreply.github.com>
2019-05-19 15:55:15 +0900

    02A-2 UPDATE
    yaya

--- 01A-1/script/menu.js
+++ 01A-1/script/menu.js
@@ -5,5 +5,16 @@ $(document).ready(function(){
 	$("#wrap #head_sec .head_ri .menus > div").mouseleave(function(){
 		$(this).children("div").addClass("on");	
 	})
-    
 })
+function popUp(){
+    $(".pop").css({
+        'opacity':'1',
+        'z-index': '100'
+    })
+}
+function popDown(){
+    $(".pop").css({
+        'opacity':'0',
+        'z-index': '-100'
+    })
+}
